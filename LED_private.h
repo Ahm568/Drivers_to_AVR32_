2f528fa170534eeac38c512ceb86e7c7a084568d
Ahm568 <ahmedmajdi1010@gmail.com>
2024-09-19 15:05:25 +0300

    LED

---
+++ LED_private.h
@@ -0,0 +1,13 @@
+/*
+ * LED_pricate.h
+ *
+ *  Created on: 3 Sep 2024
+ *      Author: User
+ */
+
+#ifndef HAL_HLED_LED_PRIVATE_H_
+#define HAL_HLED_LED_PRIVATE_H_
+
+
+
+#endif /* HAL_HLED_LED_PRIVATE_H_ */
